3d1d1935937aef3d9078f289c84afadf7fbee149
ARCH <78976392+iamarch@users.noreply.github.com>
2021-09-02 14:51:06 +0530

    Update MissRoseSong_Bot.py

--- MissRoseSong_Bot.py
+++ MissRoseSong_Bot.py
@@ -116,7 +116,7 @@ def song(client, message):
         query += ' ' + str(i)
     print(query)
     m = message.reply('🔎 Searching the song...')
-    ydl_opts = {"format": "bestaudio", "writethumbnail": True,"addmetadata": True,"geo-bypass": True,"nocheckcertificate": True,"outtmpl": "%(id)s.mp3"}
+    ydl_opts = {"format": "best","writethumbnail": True,"addmetadata": True,"geo-bypass": True,"nocheckcertificate": True,"outtmpl": "%(id)s.mp4}
     try:
         results = []
         count = 0
