66418e7a00d0d12a070e84e7850fd168e97f6264
ARCH <78976392+iamarch@users.noreply.github.com>
2021-09-04 12:42:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    BOT_TOKEN = "1955303395:AAEpazplYhGXOyuapWyW_X3WKTROMDEbTzo" 
+    BOT_TOKEN = "1955303395:AAGu7yn4oPlBqaF6Fv79sYmDLjmaa5ST0fk" 
     API_ID =  4038923
     API_HASH = "2835eefd1bc28eb0b64646038be2a471"
     
